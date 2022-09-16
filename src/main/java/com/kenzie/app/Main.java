fd7d676655d11817238858856cf7234aa4e6576e
Bryceclark-dev <brycejclark@sbcglobal.net>
2022-09-16 09:41:54 -0500

    Complete program and passed all tests

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -14,12 +14,45 @@ public class Main {
         double price = Double.parseDouble(in.nextLine());
 
         // Create your StockHolding using the values above
-
+        StockHolding stock = new StockHolding(name, ticker, price);
         // Create your Execution Loop
             // print the menu
             // collect which option they chose from Scanner
             // perform the operation: Check your balance, buy, sell, or exit
             // repeat!
+        int input = 0;
+        int numOfShares;
+        double amount;
+        while(input != 4) {
+            System.out.println("Enter a selection (1-4)\n" +
+                    "1. Check your Balance\n" +
+                    "2. Buy\n" +
+                    "3. Sell\n" +
+                    "4. Exit");
+            input = in.nextInt();
+
+            switch (input){
+                case 1:
+                    System.out.println("You own "+stock.getNumShares()+" shares of "+stock.getName()+" ("+stock.getTicker()+")\n" +
+                                        "Your balance is: $" + stock.getBalance());
+                    break;
+                case 2:
+                    System.out.println("How many shares would you like to buy?");
+                    numOfShares = in.nextInt();
+                    stock.buy(numOfShares);
+                    break;
+                case 3:
+                    System.out.println("How many shares would you like to sell?");
+                    numOfShares = in.nextInt();
+                    amount = stock.sell(numOfShares);
+                    if(amount > 0) {
+                        System.out.println("You sold " + numOfShares + " shares for $" + amount);
+                    }else{
+                        System.out.println("You do not have enough shares!");
+                    }
+
+            }
+        }
 
         System.out.println("Goodbye!");
     }
--- src/main/java/com/kenzie/app/StockHolding.java
+++ src/main/java/com/kenzie/app/StockHolding.java
@@ -2,11 +2,72 @@ package com.kenzie.app;
 
 public class StockHolding {
     // Create private instance properties
+    private String name;
+    private String ticker;
+    private double price;
+    private int numShares;
 
     // Create constructor
 
+    public StockHolding(String name, String ticker, double price) {
+        this.name = name;
+        this.ticker = ticker;
+        this.price = price;
+        this.numShares = 0;
+    }
+
     // create getters
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumShares() {
+        return numShares;
+    }
+
+    public void setNumShares(int numShares) {
+        this.numShares = numShares;
+    }
+
     // create buy, sell, and getBalance methods
+    public void buy(int shares){
+        numShares += shares;
+        System.out.println("You bought "+shares+" shares for $" + price*shares);
+    }
+
+    public double sell(int shares){
+        if(shares > numShares){
+            return 0;
+        }else{
+            numShares -= shares;
+        }
+
+        return shares * price;
+    }
+
+    public double getBalance(){
+        return numShares*price;
+    }
 
 }
